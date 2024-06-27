5a41ce0a7de8d847b2ed703d9eacbd060d4746d6
Kittanut Thammapibaljit <tkittanut@gmail.com>
2024-06-27 15:42:04 +0700

    feat: add read, update, delete method for answers route

--- routes/answers.mjs
+++ routes/answers.mjs
@@ -148,7 +148,123 @@ answersRouter.post("/:id/downvote", async (req, res) => {
 });
 
 // READ
+answersRouter.get("/:id", async (req, res) => {
+  const answerId = req.params.id;
+
+  try {
+    const result = await connectionPool.query(
+      `
+            SELECT id, question_id, content, created_at, updated_at
+            FROM answers
+            WHERE id=$1
+            `,
+      [answerId]
+    );
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({
+        message: "404 Not Found: Answer not found.",
+      });
+    }
+
+    return res.status(200).json({
+      message: "200 OK: Successfully retrieved the answer.",
+      data: result.rows[0],
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      message: "Server could not process the request due to database issue.",
+    });
+  }
+});
+
 // UPDATE
+answersRouter.put("/:id", async (req, res) => {
+  const answerId = req.params.id;
+  const { content } = req.body;
+
+  try {
+    const checkIsAnswerExist = await connectionPool.query(
+      `
+            SELECT id
+            FROM answers
+            WHERE id=$1
+            `,
+      [answerId]
+    );
+
+    if (checkIsAnswerExist.rows.length === 0) {
+      return res.status(404).json({
+        message: "404 Not Found: Answer not found.",
+      });
+    }
+
+    const result = await connectionPool.query(
+      `
+            UPDATE answers
+            SET content = $1,
+                updated_at = NOW()
+            WHERE id=$2
+            RETURNING id, question_id, content, created_at, updated_at
+            `,
+      [content, answerId]
+    );
+
+    if (result.rows.length === 0) {
+      return res
+        .status(404)
+        .json({ message: "404 Not Found: Answer not found." });
+    }
+
+    return res.status(200).json({
+      message: "200 OK: Successfully updated the answer.",
+      data: result.rows[0],
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      message: "Server could not process the request due to database issue.",
+    });
+  }
+});
 // DELETE
+answersRouter.delete("/:id", async (req, res) => {
+  const answerId = req.params.id;
+
+  try {
+    const checkIsAnswerExist = await connectionPool.query(
+      `
+        SELECT id
+        FROM answers
+        WHERE id=$1
+        `,
+      [answerId]
+    );
+
+    if (checkIsAnswerExist.rows.length === 0) {
+      return res.status(404).json({
+        message: "404 Not Found: Answer not found.",
+      });
+    }
+
+    const result = await connectionPool.query(
+      `
+        DELETE FROM answers
+        WHERE id=$1
+        `,
+      [answerId]
+    );
+
+    return res.status(200).json({
+      message: "200 OK: Answer deleted successfully.",
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      message: "Server could not process the request due to database issue.",
+    });
+  }
+});
 
 export default answersRouter;
