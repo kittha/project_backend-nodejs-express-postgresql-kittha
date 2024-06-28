bf10a984989ea00ff619cf4e9225fa16a558e534
Kittanut Thammapibaljit <tkittanut@gmail.com>
2024-06-28 07:53:43 +0700

    fix: refactor duplicate code

--- middlewares/post-put-questions.validation.mjs
+++ middlewares/post-put-questions.validation.mjs
@@ -1,21 +1,25 @@
 export const validateCreateUpdateQuestion = (req, res, next) => {
-  if (!req.body.title) {
+  const { title, description } = req.body;
+
+  if (
+    !title ||
+    typeof title !== "string" ||
+    title.length < 10 ||
+    title.length > 100
+  ) {
     return res.status(400).json({
-      message: "400 Bad Request: Missing or invalid request data.",
+      message:
+        "400 Bad Request: Invalid title. Please input between 10 to 100 characters.",
     });
   }
 
-  if (!(req.body.title.length >= 10 && req.body.title.length <= 100)) {
+  if (
+    (description && typeof description !== "string") ||
+    description.length > 300
+  ) {
     return res.status(400).json({
       message:
-        "400 Bad Request: Invalid request data. Please input between 10 to 100 characters.",
-    });
-  }
-
-  if (!(req.body.description.length <= 300)) {
-    return res.status(400).json({
-      message:
-        "400 Bad Request: Invalid request data. Please input not exceed 300 characters.",
+        "400 Bad Request: Invalid description. Please input not exceed 300 characters.",
     });
   }
 
