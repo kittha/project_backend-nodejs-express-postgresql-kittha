197355b12553d1f4e0aab6125d46b7c1b8ce5c15
Kittanut Thammapibaljit <tkittanut@gmail.com>
2024-07-01 08:14:40 +0700

    fix: fix complicate logic, add .env.example file

--- middlewares/post-put-answers.validation.mjs
+++ middlewares/post-put-answers.validation.mjs
@@ -5,7 +5,7 @@ export const validateCreateUpdateAnswer = (req, res, next) => {
     });
   }
 
-  if (!(req.body.content.length <= 300)) {
+  if (req.body.content.length > 300) {
     return res.status(400).json({
       message:
         "400 Bad Request: Invalid request data. Please input not exceed 300 characters.",
